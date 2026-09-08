28a33b92cc56bfc7e9bf405fbecc8cbae099862d
aishaabdullh1998-ui <aishaabdullh1998@gmail.com>
2026-09-08 22:01:02 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE = "bayinat-v1";
+const SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-512-maskable.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+
+  // الخطوط: من الذاكرة أولًا ثم الشبكة مع التخزين
+  if (req.url.includes("fonts.googleapis.com") || req.url.includes("fonts.gstatic.com")) {
+    e.respondWith(
+      caches.match(req).then(hit => hit || fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+        return res;
+      }).catch(() => hit))
+    );
+    return;
+  }
+
+  if (new URL(req.url).origin !== location.origin) return;
+
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy));
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
